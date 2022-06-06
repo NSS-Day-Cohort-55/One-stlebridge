f24839f5d8ae6e03524a55a8bc4278086764944c
JavontaeH <hardeman.javontae@gmail.com>
2022-06-06 11:31:33 -0500

    finished loop and added flavor text

--- src/Actions/ChooseGrazingField.cs
+++ src/Actions/ChooseGrazingField.cs
@@ -12,28 +12,37 @@ namespace Trestlebridge.Actions
         {
             Utils.Clear();
 
-            for (int i = 0; i < farm.GrazingFields.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. Grazing Field ({farm.GrazingFields[i].Animals.Count()})");
-            }
 
-            Console.WriteLine();
 
             // How can I output the type of animal chosen here?
-            Console.WriteLine($"Place the animal where?");
-
-            Console.Write("> ");
-            int choice = Int32.Parse(Console.ReadLine()) - 1;
-
-            if (farm.GrazingFields[choice].Capacity - farm.GrazingFields[choice].Animals.Count() >= 1)
+            void placeAnimal()
             {
-                farm.GrazingFields[choice].AddResource(animal);
+                for (int i = 0; i < farm.GrazingFields.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. Grazing Field ({farm.GrazingFields[i].Animals.Count()} animals)");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine($"Place the animal where?");
+
+                Console.Write("> ");
+                int choice = Int32.Parse(Console.ReadLine()) - 1;
+                if (farm.GrazingFields[choice].Capacity - farm.GrazingFields[choice].Animals.Count() >= 1)
+                {
+                    farm.GrazingFields[choice].AddResource(animal);
+                }
+                else
+                {
+                    Console.WriteLine("**** That facililty is not large enough ****");
+                    Console.WriteLine("****Please choose another one****");
+                    Console.WriteLine();
+                    placeAnimal();
+                }
             }
-            else
-            {
-                Console.WriteLine("**** That facililty is not large enough ****");
-                Console.WriteLine("****Please choose another one****");
-            }
+
+            placeAnimal();
+
+
 
             /*
                 Couldn't get this to work. Can you?
