1926f40eaa6af8cd9ed70362994e2dd91178fa0a
JavontaeH <hardeman.javontae@gmail.com>
2022-06-06 10:37:54 -0500

    fixed merge conflicts

--- src/Models/Facilities/GrazingField.cs
+++ src/Models/Facilities/GrazingField.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using Trestlebridge.Interfaces;
 
 
-namespace Trestlebridge.Models.Facilities {
+namespace Trestlebridge.Models.Facilities
+{
     public class GrazingField : IFacility<IGrazing>
     {
         private int _capacity = 50;
@@ -12,23 +13,38 @@ namespace Trestlebridge.Models.Facilities {
 
         private List<IGrazing> _animals = new List<IGrazing>();
 
-        public double Capacity {
-            get {
+        public double Capacity
+        {
+            get
+            {
                 return _capacity;
             }
         }
 
-        public void AddResource (IGrazing animal)
+        public List<IGrazing> Animals
+        {
+            get
+            {
+                return _animals;
+            }
+        }
+
+
+
+        public void AddResource(IGrazing animal)
+        {
+            // TODO: implement this...
+            _animals.Add(animal);
+            throw new NotImplementedException();
+        }
+
+        public void AddResource(List<IGrazing> animals)
         {
             // TODO: implement this...
             throw new NotImplementedException();
         }
 
-        public void AddResource (List<IGrazing> animals) 
-        {
-            // TODO: implement this...
-            throw new NotImplementedException();
-        }
+
 
         public override string ToString()
         {
